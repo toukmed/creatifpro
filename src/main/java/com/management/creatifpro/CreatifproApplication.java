faed697e87102f03593c5cc2b4e6a65010ffcc82
Mohamed Toukrichte <048548@INTELCIA.COM>
2024-01-11 10:13:41 +0100

    initial commit

---
+++ src/main/java/com/management/creatifpro/CreatifproApplication.java
@@ -0,0 +1,13 @@
+package com.management.creatifpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatifproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreatifproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/management/creatifpro/CreatifproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.management.creatifpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatifproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
