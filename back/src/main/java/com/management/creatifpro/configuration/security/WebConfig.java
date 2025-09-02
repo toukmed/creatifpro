28b0e8e484e943b1e2830d4dddecac3195320919
toukrichtemed <toukrichtemed@gmail.com>
2025-09-02 18:25:44 +0100

    add toukrichtemed.com domain

--- back/src/main/java/com/management/creatifpro/configuration/security/WebConfig.java
+++ back/src/main/java/com/management/creatifpro/configuration/security/WebConfig.java
@@ -21,10 +21,10 @@ public class WebConfig {
         config.setAllowCredentials(true);
         config.addAllowedOrigin("http://localhost:4200");
         config.addAllowedOrigin("http://72.60.91.94");
-        config.addAllowedOrigin("https://erp.creatif-pro.com/");
-        config.addAllowedOrigin("http://erp.creatif-pro.com/");
-        config.addAllowedOrigin("https://www.erp.creatif-pro.com/");
-        config.addAllowedOrigin("http://www.erp.creatif-pro.com/");
+        config.addAllowedOrigin("https://toukrichtemed.com/");
+        config.addAllowedOrigin("http://toukrichtemed.com/");
+        config.addAllowedOrigin("https://www.toukrichtemed.com/");
+        config.addAllowedOrigin("http://www.toukrichtemed.com/");
         config.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
                 "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
                 "Access-Control-Request-Method", "Access-Control-Request-Headers"));
